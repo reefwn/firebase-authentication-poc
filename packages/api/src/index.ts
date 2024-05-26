5508a98f0e9dc3b0a73c514485489c09e5848f24
Yossapol Witayanont <reef_3llen@hotmail.com>
2024-05-26 15:41:52 +0700

    feat: rocket: init

---
+++ packages/api/src/index.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001 });
+    console.log('Server listening on http://localhost:3001');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
